6471405d11793803b45a2c329c1986d59544292e
ADARBEH <retaad99@gmail.com>
2022-09-10 15:23:48 +0300

    create Append method

--- __test__/ll.test.js
+++ __test__/ll.test.js
@@ -28,3 +28,24 @@ describe('Insert to the beginning of the linked list', () => {
         expect(ll.head.next.next).toBeNull();
     })
 })
+
+describe('Append to the end of the Linked List', () => {
+    it('Append to an empty LL', () => {
+      const ll = new LinkedList();
+      ll.append('a');
+      expect(ll.head.value).toEqual('a');
+      expect(ll.head.next).toBeNull();
+    })
+  
+    it('Append to non-empty LL', () => {
+      const ll = new LinkedList();
+      ll.append('a');
+      ll.append('b');
+      ll.append('c');
+  
+      expect(ll.head.value).toEqual('a');
+      expect(ll.head.next.value).toEqual('b');
+      expect(ll.head.next.next.value).toEqual('c');
+      expect(ll.head.next.next.next).toBeNull();
+    })
+  })
